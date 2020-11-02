14234c890215528f5341445186bdc48c57952521
Kaitlyn Higa <kaitlynhiga@gmail.com>
2020-11-02 09:57:23 -0800

    Create render for GridTile
    This component was also changed to be stateless, as there is no
    state to be held on this component.

--- client/src/views/Fruits/FruitsResultsGridTile.js
+++ client/src/views/Fruits/FruitsResultsGridTile.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
 import { GridTile } from 'material-ui';
 
-export default class FruitsResultsGridTile extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <GridTile/>
-    );
-  }
+export default FruitsResultsGridTile = props => {
+  props.img = props.hasOwnProperty('img') ? props.img : "https://www.google.com/logos/doodles/2020/where-to-vote-6753651837108825-l.png"
+  
+  return(
+    <GridTile
+        title={props.fruit}
+        subtitle={<span> <b>{props.weight}</b></span>}
+      >
+        <img src={props.img} />
+      </GridTile>
+  );
 }
