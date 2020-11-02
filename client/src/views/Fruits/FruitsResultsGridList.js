835e3ca42a72233c5f5da8115c9092bd9c26ea3f
Kaitlyn Higa <kaitlynhiga@gmail.com>
2020-11-02 09:31:59 -0800

    Create GridList and GridTile components for results
    Based on the boilerplate's included material-ui GridList and GridTile components

---
+++ client/src/views/Fruits/FruitsResultsGridList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { GridList } from 'material-ui';
+
+export default class FruitsResultsGridList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <GridList>
+      </GridList>
+    );
+  }
+}
---
+++ client/src/views/Fruits/FruitsResultsGridTile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { GridTile } from 'material-ui';
+
+export default class FruitsResultsGridTile extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <GridTile/>
+    );
+  }
+}
