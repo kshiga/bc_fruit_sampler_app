486973c3b9374b56d998628f1b2db24002675db0
Kaitlyn Higa <kaitlynhiga@gmail.com>
2020-11-02 09:41:01 -0800

    Render child tiles under list

--- client/src/views/Fruits/FruitsResultsGridList.js
+++ client/src/views/Fruits/FruitsResultsGridList.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 import { GridList } from 'material-ui';
+import FruitsResultsGridTile from './FruitsResultsGridTile';
 
 export default class FruitsResultsGridList extends React.Component {
   constructor(props){
     super(props);
+
+    this.renderFruitsResults = this.renderFruitsResults.bind(this);
+  }
+
+  renderFruitsResults(){
+    return this.props.fruitsResults.map((fruit, index) => {
+      <FruitsResultsGridTile {...fruit} key={index}/>
+    })
   }
 
   render(){
     return(
       <GridList>
+        <Subheader>Query: {this.props.query}</Subheader>
+        { this.renderFruitsResults() }
       </GridList>
     );
   }
